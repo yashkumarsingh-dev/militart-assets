617bc83abc17ee83512785f8cacd9151dfcb0e9a
cooldude-69 <bca45027.20@bitmesra.ac.in>
2025-06-30 00:09:56 +0530

    mg

--- backend/src/models/asset.js
+++ backend/src/models/asset.js
@@ -4,15 +4,17 @@ const Asset = sequelize.define(
   "Asset",
   {
     id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: { type: DataTypes.STRING, allowNull: false },
     type: { type: DataTypes.STRING, allowNull: false },
-    description: { type: DataTypes.STRING },
+    description: { type: DataTypes.TEXT },
     serial_number: { type: DataTypes.STRING, unique: true },
     base_id: { type: DataTypes.INTEGER, allowNull: false },
     status: { type: DataTypes.STRING, allowNull: false },
+    value: { type: DataTypes.INTEGER, allowNull: true },
   },
   {
     tableName: "assets",
-    timestamps: false,
+    timestamps: true,
   }
 );
 
