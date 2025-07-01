8f999b1ff20edc9a77934151789d454e8cf9cb4c
cooldude-69 <yash.srs55@gmail.com>
2025-07-01 12:21:36 +0530

    Remove sensitive and irrelevant files from repo and add to .gitignore

--- backend/seed-data.js
+++
@@ -1,7 +0,0 @@
-// Add bases
-const bases = [
-  { id: 1, name: "Default Base", location: "HQ" },
-  { id: 2, name: "Bravo Base", location: "Sector B" },
-  { id: 3, name: "Delta Base", location: "Sector D" },
-  { id: 4, name: "Alpha Base", location: "Sector A" },
-];
